04aa4eb7065726e386a523ae0cc31d381de44717
Santiago Quiñones <lsquinones@gmail.com>
2021-06-16 02:41:17 +0000

    Actualización de mensaje

--- traductor/__main__.py
+++ traductor/__main__.py
@@ -4,7 +4,7 @@ from translate import Translator
 @click.command()
 @click.argument('palabra_en')
 def main(palabra_en):
-	print(f'Traduciendo {palabra_en}')
+	print(f'Traduciendo...')
 	translator = Translator(from_lang = 'english', to_lang='Spanish')
 	palabra_es = translator.translate(palabra_en)
 
